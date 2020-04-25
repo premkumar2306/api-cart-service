698f527dacf67a3d491b2e18d8c2cc5f16ac7a80
Prem <>
2020-04-25 07:05:31 -0500

    testing with customer email set

--- functions/checkout/create-stripe-checkout.js
+++ functions/checkout/create-stripe-checkout.js
@@ -8,18 +8,24 @@ const createStripeCheckout = async (cartId, customerId) => {
     console.log(cart);
     const secret = await secretManager("/external");
     const stripe = require('stripe')(secret.STRIPE_API_KEY);
-
-    const session = await stripe.checkout.sessions.create({
-        // customer:customerId,
-        payment_method_types:['card'],
-        customer_email='test1@bar.com',
-        // billing_address_collection:'required',
-        line_items: mapToStripe(cart.cartItems),
-        success_url: 'https://localhost.com:3000/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://localhost.com:3000/cancel',
-    });
-    console.log(session);
-    return {...session, ...cart};
+    try {
+        const session = await stripe.checkout.sessions.create({
+            // customer:customerId,
+            payment_method_types:['card'],
+            customer_email='test1@bar.com',
+            billing_address_collection:'required',
+            shipping_address_collection={
+                'allowed_countries': ['US', 'CA'],
+            },
+            line_items: mapToStripe(cart.cartItems),
+            success_url: 'https://localhost.com:3000/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url: 'https://localhost.com:3000/cancel',
+        });
+        console.log(session);
+        return {...session, ...cart};
+    } catch (error) {
+        return {error}
+    }
 };
 
 module.exports = createStripeCheckout;
