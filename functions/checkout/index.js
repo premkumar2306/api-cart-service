f1dbb93cf0ecf059a205481364c0b4d30b427cb7
Prem Kumar <premkumar@Prems-MacBook-Pro.local>
2020-04-27 19:28:21 -0500

    added dotenv

--- functions/checkout/index.js
+++ functions/checkout/index.js
@@ -3,10 +3,9 @@
 const createStripeCheckout = require("./create-stripe-checkout");
 
 module.exports.handler = async function (event, context) {
-    debugger;
     console.log(JSON.stringify(event));
     const cartId = event.pathParameters.cartid;
-    const customerId = '32444zxBBfRP791t'
+    const customerId = 'cus_H9tezcPBgseibW'
     const data = await createStripeCheckout(cartId, customerId);
     return { statusCode: 200, body: JSON.stringify({ data }) };
 }
