7d4a4e2f7c3262d115bd866091187579ae5f9393
Prem <premkumar.duraiswamy@rallyhealth.com>
2020-12-11 13:37:44 -0600

    added description

--- helper/mapCartItems.js
+++ helper/mapCartItems.js
@@ -19,6 +19,7 @@ const mapCartItems = function (cartItems) {
       brand: c.brand,
       quantity: c.quantity,
       title: c.title,
+      description: c.description,
       images: c.images,
       category: c.category,
       price: c.price,
