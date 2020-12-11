723e5208f986816d22624dbf8abf03b815a284ed
Prem <premkumar.duraiswamy@rallyhealth.com>
2020-12-10 18:40:06 -0600

    updated mapping

--- functions/checkout/index.js
+++ functions/checkout/index.js
@@ -4,19 +4,22 @@ const dynamodb = require('../dynamodb');
 
 module.exports.handler = async function (event) {
   console.log(JSON.stringify(event));
-  const cartId = event.pathParameters.cartid;
   const customerId = "cus_H9tezcPBgseibW";
   const body = JSON.parse(event.body);
+  if (!body || !body.checkoutInput) {
+    return { statusCode: 501, body: JSON.stringify('Cart ID not found') };
+  }
+  const cartId = body.checkoutInput.pk || body.checkoutInput.id;
   console.log(
     `cartId: ${cartId} checkout process began at ${new Date().toISOString()}`
   );
   const cart = await getCart(cartId);
   const checkout = { ...cart };
-  if (body && body.razorpay_payment_id) {
+  if (body && body.checkoutInput && body.checkoutInput.razorpay_payment_id) {
     checkout.payment = {
-      razorpay_order_id: body.razorpay_order_id,
-      razorpay_payment_id: body.razorpay_payment_id,
-      razorpay_signature: body.razorpay_signature
+      razorpay_order_id: body.checkoutInput.razorpay_order_id,
+      razorpay_payment_id: body.checkoutInput.razorpay_payment_id,
+      razorpay_signature: body.checkoutInput.razorpay_signature
     };
   } else {
     const razorOrder = await createRazorPayCheckout(checkout, cartId, customerId);
